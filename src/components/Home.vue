763646fc59831b33cad9913afd7b03ded79a061e
ingalls <nick@ingalls.ca>
2023-07-25 08:42:08 -0600

    Display Logo

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,6 +2,8 @@
 <div class='h-100'>
     <div style="background: #eee url(/MainPage.jpg) no-repeat 0 0; background-size: cover;" class="h-100">
         <div class='d-flex px-3 py-3'>
+            <div style="background: url(/logo.png) no-repeat 0 0; background-size: cover; width: 50px; height: 50px;"></div>
+            <div class=''>Mesa County Search &amp; Rescue</div>
             <div class='ms-auto btn-list'>
                 <LoginIcon @click='external("https://team.mesacountysar.com")' v-tooltip='"Team Login"' class='cursor-pointer'/>
             </div>
