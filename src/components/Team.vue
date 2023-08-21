05f2e0a570d6b2d27847b2dc6023acbf5b6851ce
ingalls <nick@ingalls.ca>
2023-08-21 10:30:56 -0600

    Leadership Page

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -4,6 +4,23 @@
 
     <Header label='Our Team'/>
 
+    <div class='container-xl mt-6'>
+        <div class='row row-cards'>
+            <div class="col-md-6 col-lg-3">
+                <div class="card">
+                    <div class="card-body p-4 text-center">
+                        <span class="avatar avatar-xl mb-3 rounded" style="background-image: url(./static/avatars/000m.jpg)"></span>
+                        <h3 class="m-0 mb-1"><a href="#">Paweł Kuna</a></h3>
+                        <div class="text-secondary">UI Designer</div>
+                        <div class="mt-3">
+                          <span class="badge bg-purple-lt">Owner</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <PageFooter/>
 </div>
 </template>
