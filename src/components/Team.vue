e865f2ccc4f37d64802747af61dc0f848cd8e854
ingalls <nick@ingalls.ca>
2023-08-21 10:43:53 -0600

    Sketch out Leadership

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -3,18 +3,58 @@
     <Call911 :menu='false'/>
 
     <Header label='Our Team'/>
+    <div class='container-xl mt-6 mb-6'>
+        <div class="row g-2 align-items-center">
+            <div class="col d-flex">
+                <TablerBreadCrumb/>
+            </div>
+        </div>
+    </div>
 
-    <div class='container-xl mt-6'>
+    <div class='container-xl'>
         <div class='row row-cards'>
+            <h2>Board</h2>
+
             <div class="col-md-6 col-lg-3">
+                <Leader name='Nate Rinderle' group='Board' position='President'/>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <Leader name='Garrett Rodstrom' group='Board' position='Vice President'/>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <Leader name='Donna Powelson' group='Board' position='Treasurer'/>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <Leader name='Nathan Smithgall' group='Board' position='Secretary'/>
+            </div>
+        </div>
+
+        <div class='row row-cards mt-3'>
+            <h2>Standards</h2>
+
+            <div class="col-md-6 col-lg-3">
+                <Leader name='Joe Kanopsic' group='Standards' position='Standards'/>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <Leader name='Nicholas Ingalls' group='Standards' position='Standards'/>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <Leader name='Jeanine Camp' group='Standards' position='Standards'/>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <Leader name='Jeffrey Stephens' group='Standards' position='Standards'/>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <Leader name='Cory Lidberg' group='Standards' position='Standards'/>
+            </div>
+        </div>
+
+        <div class='row row-cards mt-3'>
+            <h2>Our Team</h2>
+
+            <div class="col-12">
                 <div class="card">
                     <div class="card-body p-4 text-center">
-                        <span class="avatar avatar-xl mb-3 rounded" style="background-image: url(./static/avatars/000m.jpg)"></span>
-                        <h3 class="m-0 mb-1"><a href="#">Paweł Kuna</a></h3>
-                        <div class="text-secondary">UI Designer</div>
-                        <div class="mt-3">
-                          <span class="badge bg-purple-lt">Owner</span>
-                        </div>
                     </div>
                 </div>
             </div>
@@ -29,6 +69,11 @@
 import Call911 from './util/Call911.vue';
 import Header from './util/Header.vue';
 import PageFooter from './util/PageFooter.vue';
+import Leader from './util/Leader.vue';
+
+import {
+    TablerBreadCrumb
+} from '@tak-ps/vue-tabler';
 
 export default {
     name: 'Team',
@@ -37,9 +82,11 @@ export default {
         }
     },
     components: {
+        Leader,
         Call911,
         Header,
         PageFooter,
+        TablerBreadCrumb
     }
 }
 </script>
