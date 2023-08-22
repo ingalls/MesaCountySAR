6a4526ed8c84ef0fcd53ddf2dd1eb353c86eb752
ingalls <nick@ingalls.ca>
2023-08-22 07:44:38 -0600

    Add Leader Card

---
+++ src/components/util/Leader.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="card">
+    <div class="card-body p-4 text-center">
+        <span class="avatar avatar-xl mb-3 rounded" style="background-image: url(./static/avatars/000m.jpg)"></span>
+        <h3 class="m-0 mb-1" v-text='name'></h3>
+        <div class="text-secondary" v-text='position'></div>
+        <div class="mt-3">
+          <span class="badge bg-purple-lt" v-text='group'></span>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Leader',
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        position: {
+            type: String,
+            required: true
+        },
+        group: {
+            type: String,
+            required: true
+        }
+    },
+}
+</script>
+
